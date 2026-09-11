2c4c69973317442023c3deb76afee8f0dd3bcb7b
Esteban Alfaro <esteban@sentientagency.io>
2026-09-11 12:25:46 -0600

    Publish Dev-only account requests and complete site assets

---
+++ assets/dashboard-CSiAN95a.js
@@ -0,0 +1,1 @@
+import{R as e,j as t,a as r}from"./urlCodec-DrM8WhyB.js";import{A as o}from"./styles-CDIhz_oU.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
