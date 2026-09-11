4465f24a34bf310f88c328786a987211cebbb110
Esteban Alfaro <esteban@sentientagency.io>
2026-09-11 13:06:19 -0600

    Publish account request experience

--- assets/dashboard-CSiAN95a.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as t,a as r}from"./urlCodec-DrM8WhyB.js";import{A as o}from"./styles-CDIhz_oU.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
